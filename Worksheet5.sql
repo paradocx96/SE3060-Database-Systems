63e2cf27fb0339da21b058f7439062a3206c6537
Navinda Lankesh <adilanga@gmail.com>
2021-09-23 15:22:57 +0530

    Worksheet Added

---
+++ Worksheet5.sql
@@ -0,0 +1,214 @@
+-- PL / SQL --
+-- Worksheet 5 --
+
+-- Simple Print Line
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('This is my first PL/SQL Program');
+END;
+/
+
+-- Declare & Assign vales
+DECLARE
+    text VARCHAR(50) := 'This is my first PL/SQL Program';
+BEGIN
+    DBMS_OUTPUT.PUT_LINE(text);
+END;
+/
+
+-- CREATE TABLE CUSTOMERS
+-- (
+--     ID      INT         NOT NULL,
+--     NAME    VARCHAR(20) NOT NULL,
+--     AGE     INT         NOT NULL,
+--     ADDRESS CHAR(25),
+--     SALARY  DECIMAL(18, 2),
+--     PRIMARY KEY (ID)
+-- );
+-- INSERT INTO CUSTOMERS (ID, NAME, AGE, ADDRESS, SALARY)
+-- VALUES (1, 'Ramesh', 32, 'Colombo', 2000.00);
+-- INSERT INTO CUSTOMERS (ID, NAME, AGE, ADDRESS, SALARY)
+-- VALUES (2, 'Kamal', 25, 'Kandy', 1500.00);
+-- INSERT INTO CUSTOMERS (ID, NAME, AGE, ADDRESS, SALARY)
+-- VALUES (3, 'Saman', 23, 'Piliyandala', 2000.00);
+-- INSERT INTO CUSTOMERS (ID, NAME, AGE, ADDRESS, SALARY)
+-- VALUES (4, 'Kapila', 25, 'Matara', 6500.00);
+-- INSERT INTO CUSTOMERS (ID, NAME, AGE, ADDRESS, SALARY)
+-- VALUES (5, 'Harsha', 27, 'Maharagama', 8500.00);
+-- INSERT INTO CUSTOMERS (ID, NAME, AGE, ADDRESS, SALARY)
+-- VALUES (6, 'Janith', 22, 'Hatton', 4500.00);
+
+-- Example 2
+DECLARE
+	var_cname varchar(12);
+	var_cid int := 2;
+BEGIN
+    SELECT c.name INTO var_cname
+	FROM  CUSTOMERS c
+	WHERE c.ID = var_cid;
+    DBMS_OUTPUT.PUT_LINE('Name of the customer with cid : ' || var_cid || ' is ' || var_cname );
+END;
+/
+
+-- Exercise 01
+-- Write a PL/SQL to get the current price of a stock belongs to company ‘IBM’.
+-- Store the company name (‘IBM’) in a variable.
+SELECT * FROM STOCKS;
+
+DECLARE
+    current_price FLOAT;
+    company_name VARCHAR(5) := 'IBM';
+BEGIN
+    SELECT s.CURRENTPRICE INTO current_price
+    FROM STOCKS s
+    WHERE s.COMPANY = company_name;
+    DBMS_OUTPUT.PUT_LINE('Company Name : ' || company_name || ', Current Price : ' || current_price);
+END;
+/
+
+-- RECORDS EXAMPLE
+DECLARE
+    cus_rec CUSTOMERS%ROWTYPE;
+    var_cid CUSTOMERS.ID%TYPE := 2;
+BEGIN
+    SELECT * INTO cus_rec
+    FROM CUSTOMERS c
+    WHERE c.ID = var_cid;
+    DBMS_OUTPUT.PUT_LINE('Customer ID : ' || cus_rec.ID );
+    DBMS_OUTPUT.PUT_LINE('Name : ' || cus_rec.name );
+    DBMS_OUTPUT.PUT_LINE('Address : ' || cus_rec.address );
+END;
+/
+
+--  Exercise 02
+-- Write a PL/SQL to display a message for a given stock of ‘IBM’
+-- company according to the following criteria.
+-- Current price < 45 - ‘Current price is very low !’
+-- 45 <= Current price < 55 - ‘Current price is low !’
+-- 55 <= Current price < 65 - ‘Current price is medium !’
+-- 65 <= Current price < 75 - ‘Current price is medium high !’
+-- 75 <= Current price - ‘Current price is high !’
+-- Store the company name in a variable.
+SELECT * FROM STOCKS;
+
+DECLARE
+    current_price FLOAT;
+    company_name VARCHAR(5) := 'IBM';
+BEGIN
+    SELECT s.CURRENTPRICE INTO current_price
+    FROM STOCKS s
+    WHERE s.COMPANY = company_name;
+    IF (current_price < 45) THEN
+        DBMS_OUTPUT.PUT_LINE('Current price is very low !');
+    ELSIF (45 <= current_price AND current_price < 55) THEN
+        DBMS_OUTPUT.PUT_LINE('Current price is low !');
+    ELSIF (55 <= current_price AND current_price < 65) THEN
+        DBMS_OUTPUT.PUT_LINE('Current price is medium !');
+    ELSIF (65 <= current_price AND current_price < 75) THEN
+        DBMS_OUTPUT.PUT_LINE('Current price is medium high !');
+    ELSIF (75 <= current_price) THEN
+        DBMS_OUTPUT.PUT_LINE('Current price is high !');
+    end if;
+    DBMS_OUTPUT.PUT_LINE('Company Name : ' || company_name || ', Current Price : ' || current_price);
+END;
+/
+
+-- Simple Loop
+DECLARE
+    x NUMBER := 10;
+BEGIN
+    LOOP
+        DBMS_OUTPUT.PUT_LINE(x);
+        x := x + 10;
+        EXIT WHEN x > 50;
+    END LOOP;
+    DBMS_OUTPUT.PUT_LINE('AFTER ALL X=' || x);
+END;
+/
+
+--  While Loop
+DECLARE
+    x NUMBER := 10;
+BEGIN
+    WHILE x < 50 LOOP
+        DBMS_OUTPUT.PUT_LINE('Value X=' || x);
+        x := x + 10;
+    END LOOP;
+END;
+/
+
+-- FOR Loop
+DECLARE
+    x NUMBER;
+BEGIN
+    FOR x IN 1..10 LOOP
+        DBMS_OUTPUT.PUT_LINE('VALUE X=' || x);
+    END LOOP;
+END;
+/
+
+-- Labels
+begin
+    <<i_loop>> for i in 1 .. 10 loop
+        <<j_loop>> for j in 1 .. 10 loop
+            dbms_output.put(to_char(j, '999'));
+        exit j_loop when j=i;
+        end loop;
+        dbms_output.new_line;
+    end loop;
+end;
+/
+
+-- Exercise 03
+-- Write three PL/SQLs to draw the following shape. Use all three types of loops in each PL/SQL.
+-- 9 9 9 9 9 9 9 9 9
+-- 8 8 8 8 8 8 8 8
+-- 7 7 7 7 7 7 7
+-- 6 6 6 6 6 6
+-- 5 5 5 5 5
+-- 4 4 4 4
+-- 3 3 3
+-- 2 2
+-- 1
+BEGIN
+    FOR x IN REVERSE 1..9 LOOP
+        FOR y IN  1..x LOOP
+            DBMS_OUTPUT.PUT(x ||' ');
+        END LOOP;
+        DBMS_OUTPUT.NEW_LINE;
+    end loop;
+end;
+/
+
+-- Cursors
+-- Implicit Cursors
+-- The cursor attributes available are
+-- %FOUND, %NOTFOUND, %ROWCOUNT, %ISOPEN
+
+DECLARE
+    CURSOR stock_cur IS
+        SELECT s.COMPANY, s.CURRENTPRICE
+        FROM STOCKS s;
+    stock_rec stock_cur%rowtype;
+BEGIN
+    FOR stock_rec in stock_cur LOOP
+        dbms_output.put_line('Company : ' || stock_rec.COMPANY || ' - Current Price : ' ||stock_rec.CURRENTPRICE);
+    END LOOP;
+END;
+/
+
+-- Explicit Cursors
+DECLARE
+    CURSOR cus_cur IS
+        SELECT * FROM CUSTOMERS c WHERE c.SALARY > 4000;
+    cus_rec cus_cur%ROWTYPE;
+BEGIN
+    IF NOT cus_cur%ISOPEN THEN
+        OPEN cus_cur;
+    END IF;
+    LOOP
+        FETCH cus_cur INTO cus_rec;
+        EXIT WHEN cus_cur%NOTFOUND;
+        DBMS_OUTPUT.PUT_LINE('NAME :' || cus_rec.NAME || ', SALARY:' || cus_rec.SALARY);
+    END LOOP;
+END;
+/
---
+++ XML Lecture.sql
@@ -0,0 +1,210 @@
+-- Create Table with untyped XML --
+
+CREATE TABLE AdminDocs (
+id int primary key,
+xDoc XML not null
+);
+
+-- Example: Inserting Data into Untyped XML Column --
+
+INSERT INTO AdminDocs VALUES (1,
+'<catalog>
+<product dept="WMN">
+<number>557</number>
+<name language="en">Fleece Pullover</name>
+<colorChoices>navy black</colorChoices>
+</product>
+<product dept="ACC">
+<number>563</number>
+<name language="en">Floppy Sun Hat</name>
+</product>
+<product dept="ACC">
+<number>443</number>
+<name language="en">Deluxe Travel Bag</name>
+</product>
+<product dept="MEN">
+<number>784</number>
+<name language="en">Cotton Dress Shirt</name>
+<colorChoices>white gray</colorChoices>
+<desc>Our <i>favorite</i> shirt!</desc>
+</product>
+</catalog>'
+);
+
+INSERT INTO AdminDocs VALUES (2,
+'<doc id="123">
+<sections>
+<section num="1"><title>XML Schema</title></section>
+<section num="3"><title>Benefits</title></section>
+<section num="4"><title>Features</title></section>
+</sections>
+</doc>'
+);
+
+select * from AdminDocs;
+
+-- Example: Using Query() Method
+
+SELECT id, xDoc.query('/catalog')
+FROM AdminDocs;
+
+SELECT id, xDoc.query('/catalog/product')
+FROM AdminDocs;
+
+SELECT id, xDoc.query('/catalog/product/number')
+FROM AdminDocs;
+
+SELECT id, xDoc.query('/catalog/product/name')
+FROM AdminDocs;
+
+SELECT id, xDoc.query('/catalog/product/colorChoices')
+FROM AdminDocs;
+
+SELECT id, xDoc.query('//product')
+FROM AdminDocs;
+
+SELECT id, xDoc.query('/*/product')
+FROM AdminDocs;
+
+SELECT id, xDoc.query('/*/product[@dept="WMN"]')
+FROM AdminDocs;
+
+-- CUSTOM -- 
+
+DECLARE @x XML
+SET @x = '<ROOT><A>111</A></ROOT>'
+SELECT @x.query('ROOT/A');
+
+-- Axes --
+
+SELECT id, xDoc.query('/*/child::product[attribute::dept="WMN"]')
+FROM AdminDocs
+
+SELECT id, xDoc.query('//child::product[attribute::dept="WMN"]')
+FROM AdminDocs
+
+
+SELECT id, xDoc.query('//product[@dept="WMN"]')
+FROM AdminDocs
+
+SELECT id, xDoc.query('descendant-or-self::product[attribute::dept="WMN"]')
+FROM AdminDocs
+
+-- Print number grater than 500 result sections with all details
+SELECT id, xDoc.query('//product[number > 500]')
+FROM AdminDocs
+where id=1
+
+-- Print number grater than 500 result section's number lines (current node value > 500)
+SELECT id, xDoc.query('//product/number[. gt 500]')
+FROM AdminDocs
+where id=1
+
+-- Print 4th section
+SELECT id, xDoc.query('/catalog/product[4]')
+FROM AdminDocs
+where id=1
+
+-- Multiple Conditions
+SELECT id, xDoc.query('//product[number > 500][@dept="ACC"]')
+FROM AdminDocs
+where id=1
+
+-- Print result 1st section
+SELECT id, xDoc.query('//product[number > 500][1]')
+FROM AdminDocs
+where id=1
+
+
+-- FLWOR EXPRESSION 
+-- FOR = getting values (Like FOR Loop)
+-- LET = declare variable
+-- WHERE = conditions
+-- RETURN = return result
+
+SELECT xDoc.query(
+'for $prod in //product
+let $x:=$prod/number
+return $x')
+FROM AdminDocs
+where id=1
+
+SELECT xDoc.query(
+'for $prod in //product
+let $x:=$prod/number
+where $x>500
+return $x')
+FROM AdminDocs
+where id=1
+
+SELECT xDoc.query(
+'for $prod in //product
+let $x:=$prod/number
+where $x>500
+return (<Item>{$x}</Item>)')
+FROM AdminDocs
+where id=1
+
+SELECT xDoc.query(
+'for $prod in //product[number > 500]
+let $x:=$prod/number
+where $x>500
+return (<Item>{$x}</Item>)')
+FROM AdminDocs
+where id=1
+
+SELECT xDoc.query(
+'for $prod in //product
+let $x:=$prod/number
+where $x>500
+return (<Item>{data($x)}</Item>)')
+FROM AdminDocs
+where id=1
+
+SELECT xDoc.query(
+'for $prod in //product
+let $x:=$prod/number
+return if ($x>500)
+then <book>{$x}</book>
+else <paper>{$x}</paper>')
+FROM AdminDocs
+where id=1
+
+SELECT xDoc.query(
+'for $prod in //product
+let $x:=$prod/number
+return if ($x>500)
+then <book>{data($x)}</book>
+else <paper>{data($x)}</paper>')
+FROM AdminDocs
+where id=1
+
+
+-- DML 
+-- Example: Using Exist() Method
+SELECT id
+FROM AdminDocs
+WHERE xDoc.exist('/doc[@id = 123]') = 1
+
+--Example: Using Value() Method
+SELECT xDoc.value('(/doc//section[@num = 3]/title)[1]', 'varchar(100)')
+FROM AdminDocs
+
+--Example: Insertion of Subtree into XML Instances
+select * 
+from AdminDocs 
+where id=2
+
+-- Adding XML part
+UPDATE AdminDocs SET xDoc.modify(
+'insert
+<section num="2">
+<title>Background</title>
+</section>
+after (/doc//section[@num=1])[1]'
+);
+
+-- Delete XML part
+UPDATE AdminDocs SET xDoc.modify(
+'delete //section[@num="2"]'
+)
